75825253e1d1b46a4492c703798a939d6aa705fd
ala-boutabia <alahaya36@gmail.com>
2026-04-10 11:22:31 +0100

    create start pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import {Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Register from './pages/auth/Register'
+import Login from './pages/auth/Login'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
-    <div className='text-red-600'>
-      <h1>Real Estate Marketplace</h1>
-    </div>
+    <Routes>
+      <Route path='/' element={<Home /> } />
+      <Route path='/register' element={<Register /> } />
+      <Route path='/login' element={<Login /> } />
+      <Route path='/about' element={<About /> } />
+      <Route path='/profile' element={<Profile /> } />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   )
 }
 
