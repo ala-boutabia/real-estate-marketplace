6c8b72e6aaf0c67ad5b1b9e1775bdbc164625647
ala-boutabia <alahaya36@gmail.com>
2026-04-11 13:32:57 +0100

    Connect the mongodb database

--- api/index.js
+++ api/index.js
@@ -1,7 +1,24 @@
-import express from "express"
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () => {
-    console.log(`server is running on port 3000`)
-})
+const PORT = process.env.PORT || 3000;
+
+const startServer = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Connected to Mongodb");
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to connect to MongoDB", err.message);
+    process.exit(1);
+  }
+};
+
+startServer();
